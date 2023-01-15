2ac41f0a747ff2ca2583642d7541e6dacc039af3
Antonio Infantes <77244594+InfantesDev@users.noreply.github.com>
2023-01-16 00:07:12 +0100

    Interfaz Perfil Con Mini Menu

--- app/src/main/java/com/example/proyectogestionreservas/PerfilFragment.java
+++ app/src/main/java/com/example/proyectogestionreservas/PerfilFragment.java
@@ -2,6 +2,7 @@ package com.example.proyectogestionreservas;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -14,9 +15,15 @@ public class PerfilFragment extends Fragment {
     }
 
     @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_perfil, container, false);
+        View view= inflater.inflate(R.layout.fragment_perfil, container, false);
+        return view;
     }
 }
